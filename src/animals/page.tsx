3bf9968413b38f2490c27ebc89cbae03a82950db
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-21 10:24:46 +0000

    dynamic pages

--- src/animals/page.tsx
+++ src/animals/page.tsx
@@ -1,7 +1,43 @@
+import Link from "next/link";
+
+type petsType = {
+  name: string;
+  id: number;
+};
+
+let pets: petsType[] = [
+  {
+    name: "Joni",
+    id: 0,
+  },
+  {
+    name: "Kam",
+    id: 1,
+  },
+  {
+    name: "Libby",
+    id: 2,
+  },
+  {
+    name: "Anastatia",
+    id: 3,
+  },
+];
+
+type petParam = { params: { pets: string } };
+
 export default function Page() {
   return (
     <div>
-      <h2>Webpage about animals</h2>
+      <h4>This is a website about animals: here are many animals!</h4>
+      {pets.map((pet) => {
+        return (
+          <div key={pet.id}>
+            <br />
+            <Link href={`/animals/${pet.name.toLowerCase()}`}>{pet.name}</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
