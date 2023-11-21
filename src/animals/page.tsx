b584d2562a8d9dcb8f694df174f04f892996cecb
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-21 12:46:35 +0000

    links

--- src/animals/page.tsx
+++ src/animals/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+type petsSearchQuery = {
+  sortBy: string;
+};
+
 type petsType = {
   name: string;
   id: number;
@@ -24,12 +28,41 @@ let pets: petsType[] = [
   },
 ];
 
-type petParam = { params: { pets: string } };
+// this compares each object in pets array
+// If object a is < object b, it will swap them in the array
 
-export default function Page() {
+function comparePets(a: petsType, b: petsType) {
+  if (a.name < b.name) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// next js 'exposes' search params to us (search params being the ?query-something in URL)
+export default function Page({
+  searchParams,
+}: {
+  searchParams: petsSearchQuery;
+}) {
+  let sortedPets = [...pets];
+
+  if (searchParams.sortBy == "asc") {
+    pets.sort(comparePets);
+  } else if (searchParams.sortBy == "desc") {
+    pets.sort(comparePets).reverse();
+  }
   return (
     <div>
       <h4>This is a website about animals: here are many animals!</h4>
+      <p>{searchParams.sortBy}</p>
+      <br />
+      <Link href="/animals">Remove the sort</Link>
+      <br />
+      <Link href="/animals?sortBy=asc">Sort by ascending</Link>
+      <Link href="/animals?sortBy=dec">Sort by descending</Link>
       {pets.map((pet) => {
         return (
           <div key={pet.id}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1> This is home page</h1>
-      <Link href="/animals">Go to whatever page</Link>
+      <Link href="/animals">Go to my animals</Link>
     </div>
   );
 }
